21de809c4f90ab41172d5f3b33e41c1d8544d7d7
Cameron <cameron.fisher@absa.africa>
2021-05-13 15:03:56 +0200

    Added the ability to get su path and run root commands.
    - activity_main.xml: Added power, reboot and install apk buttons.
    - build.gradle: Added viewbinding
    - colors.xml: Added red color.
    - GeneralUtils.kt: Added ability to make toast message.
    - MainActivity.kt: Added button listeners.
    - RootUtils.kt: Added the ability to get path to su.
    - strings.xml: Added strings for buttons.

--- app/src/main/java/com/example/root/MainActivity.kt
+++ app/src/main/java/com/example/root/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.example.root
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.DocumentsContract
+import androidx.appcompat.app.AppCompatActivity
+import com.example.root.databinding.ActivityMainBinding
+import com.example.root.utils.GeneralUtils
+import com.example.root.utils.RootUtils
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        this.binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(this.binding.root)
+
+        wireUI()
+    }
+
+    private fun wireUI() {
+        with(binding) {
+            powerOffButton.setOnClickListener {
+                if (RootUtils.getSuPath() != null) {
+                    try {
+                        val process = Runtime.getRuntime().exec(arrayOf(RootUtils.getSuPath(), "-c", "reboot -p"))
+                        process.waitFor()
+                    } catch (ex: Exception) {
+                        ex.printStackTrace()
+                        GeneralUtils.makeToast(applicationContext, "Device is not rooted.")
+                    }
+                } else {
+                    GeneralUtils.makeToast(applicationContext, "Device is not rooted.")
+                }
+            }
+
+            rebootButton.setOnClickListener {
+                if (RootUtils.getSuPath() != null) {
+                    try {
+                        val process = Runtime.getRuntime().exec(arrayOf(RootUtils.getSuPath(), "-c", "reboot"))
+                        process.waitFor()
+                    } catch (ex: Exception) {
+                        ex.printStackTrace()
+                        GeneralUtils.makeToast(applicationContext, "Device is not rooted.")
+                    }
+                } else {
+                    GeneralUtils.makeToast(applicationContext, "Device is not rooted.")
+                }
+            }
+
+            installApkButton.setOnClickListener {
+                if (RootUtils.getSuPath() != null) {
+                    try {
+                        val process = Runtime.getRuntime().exec(arrayOf(RootUtils.getSuPath(), "-c", "pm install pathToApk.apk"))
+                        process.waitFor()
+                    } catch (ex: Exception) {
+                        ex.printStackTrace()
+                        GeneralUtils.makeToast(applicationContext, "Device is not rooted.")
+                    }
+                } else {
+                    GeneralUtils.makeToast(applicationContext, "Device is not rooted.")
+                }
+            }
+        }
     }
 }
